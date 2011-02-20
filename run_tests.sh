74f339eca32b61779b95cc860aa5d97e92a72ad4
Tomasz Fluxid Kowalczyk <accreg-git@fluxid.pl>
2011-02-20 17:17:22 +0100

    Removed PYTHONPATH from runtests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,5 @@
 WORKING_DIR="$(cd "${0%/*}" 2>/dev/null; dirname "$PWD"/"${0##*/}")"
 cd ${WORKING_DIR}
 
-export PYTHONPATH="${PWD}/src:${PWD}/../fxd__minilexer/src:${PYTHONPATH}"
 nosetests --with-coverage3 --cover3-package=pyhaa --doctest-tests --with-doctest tests
 #nosetests --with-coverage3 -s --pdb --pdb-failures --cover3-package=pyhaa --doctest-tests --with-doctest tests
