66f4bd677fb8a153fb33f3920ddf2269a6b52d5b
Tomasz Fluxid Kowalczyk <accreg-git@fluxid.pl>
2011-05-15 21:32:25 +0200

    Cleaned python-code matcher a bit...

--- pyhaa/parsing/matchers.py
+++ pyhaa/parsing/matchers.py
@@ -43,15 +43,32 @@ RE_EMPTY_BRACKETS = {
     for left, right in PAIRED_BRACKETS.items()
 }
 
-class PythonBracketMatcher(Matcher):
-    bracket = None
+
+class ConstantLength(Matcher):
+    def __init__(self, orig, length):
+        self.orig = orig
+        self.length = length
+        super().__init__()
+
+    def match(self, parser, line, pos):
+        result = self.orig.match(parser, line, pos)
+        if result is not None:
+            _, result = result
+            return pos + self.length, result
+        return None
+        
+
+class PythonExpressionBaseMatcher(Matcher):
+    match_bracket = None
+    ast_check = True
 
     def match(self, parser, line, pos):
         parser.cache_push()
 
         # We're not interested in what was before
         line = line[pos:]
-        assert self.bracket in L_BRACKETS and line[0] == self.bracket
+        if self.match_bracket:
+            assert self.match_bracket in L_BRACKETS and line[0] == self.match_bracket
         lines = []
 
         def linegen():
@@ -75,6 +92,9 @@ class PythonBracketMatcher(Matcher):
                 if not srow:
                     continue
 
+                if ttype in (tokenize.NEWLINE, tokenize.ENDMARKER):
+                    break
+
                 if ttype == tokenize.OP:
                     if tstring in L_BRACKETS:
                         stack.append(PAIRED_BRACKETS[tstring])
@@ -84,121 +104,14 @@ class PythonBracketMatcher(Matcher):
                             parser,
                             dict(current_pos = pos + scol),
                         )
-                if not stack:
-                    break
-        except tokenize.TokenError:
-            # Ignore. Let ast parse it again, actual error may be different
-            pass
-
-        #import pdb; pdb.set_trace()
-
-        lines = lines[:srow]
-        lines[-1] = lines[-1][:ecol]
-
-        # FIXME Tokenizer reads a bit ahead... Deal with it better than below
-        parser.cache_pop()
-        for i in range(len(lines)-1):
-            parser.readline()
-
-        jlines = (''.join(lines)+'\n')
-        jlines2 = jlines.encode('utf8')
-
-        with clear_exception_context(PyhaaSyntaxError):
-            try:
-                ast_tree = ast.parse(jlines2)
-            except SyntaxError as e:
-                # Offset is for bytes, not string characters
-                # Even if I give normal string to parse, I'll get invalid
-                # offset if line has unicode characters before it
-                fragment = jlines2[:e.offset].decode('utf8')
-                # substract 1 because python parser counts from one, and we
-                # count from zero
-                offset = len(fragment) - 1
-                raise PyhaaSyntaxError(
-                    SYNTAX_INFO.PYTHON_SYNTAX_ERROR,
-                    parser,
-                    dict(current_pos = pos + offset),
-                    desc = e.msg,
-                )
-
-        self.check_ast(parser, line, pos, ast_tree)
-
-        result = jlines
-        if RE_EMPTY_BRACKETS[self.bracket].match(result):
-            result = self.bracket + PAIRED_BRACKETS[self.bracket]
-
-        if len(lines) > 1:
-            pos = 0
-
-        return pos + len(lines[-1]), result
-
-    def check_ast(self, parser, line, pos, ast_tree):
-        pass
-
-
-class PythonDictMatcher(PythonBracketMatcher):
-    bracket = '{'
-
-    def check_ast(self, parser, line, pos, ast_tree):
-        if not isinstance(ast_tree.body[0].value, (ast.Dict, ast.DictComp)):
-            raise PyhaaSyntaxError(
-                SYNTAX_INFO.INVALID_PYTHON_ATTRIBUTES,
-                parser,
-            )
-
-
-class ConstantLength(Matcher):
-    def __init__(self, orig, length):
-        self.orig = orig
-        self.length = length
-        super().__init__()
-
-    def match(self, parser, line, pos):
-        result = self.orig.match(parser, line, pos)
-        if result is not None:
-            _, result = result
-            return pos + self.length, result
-        return None
-        
-
-class PythonLineMatcher(Matcher):
-    ast_check = True
-
-    def match(self, parser, line, pos):
-        parser.cache_push()
-
-        # We're not interested in what was before
-        line = line[pos:]
-        lines = []
-
-        def linegen():
-            yield line
-            while True:
-                yield parser.readline()
-        lineiter = linegen()
-        
-        def readline():
-            current_line = next(lineiter, '')
-            lines.append(current_line)
-            return current_line.encode('utf8')
-
-        erow = 1
-        ecol = 0
-
-        try:
-            for token in tokenize.tokenize(readline):
-                ttype, tstring, (srow, scol), (erow, ecol), tline = token
-                if not srow:
-                    continue
-
-                if ttype in (tokenize.NEWLINE, tokenize.ENDMARKER):
+                    elif tstring == ':' and not stack:
+                        break
+                if self.match_bracket and not stack:
                     break
         except tokenize.TokenError:
             if self.ast_check:
                 # Ignore. Let ast parse it again, actual error may be different
                 pass
-
-        #import pdb; pdb.set_trace()
 
         lines = lines[:srow]
         lines[-1] = lines[-1][:ecol].rstrip()
@@ -208,10 +121,10 @@ class PythonLineMatcher(Matcher):
         for i in range(len(lines)-1):
             parser.readline()
 
-        jlines = (''.join(lines)+'\n')
+        jlines = ''.join(lines)
 
         if self.check_ast:
-            jlines2 = jlines.encode('utf8')
+            jlines2 = (jlines+'\n').encode('utf8')
             with clear_exception_context(PyhaaSyntaxError):
                 try:
                     ast_tree = ast.parse(jlines2)
@@ -232,18 +145,31 @@ class PythonLineMatcher(Matcher):
 
             self.check_ast(parser, line, pos, ast_tree)
 
-        result = jlines
+        if self.match_bracket:
+            if RE_EMPTY_BRACKETS[self.match_bracket].match(jlines):
+                jlines = self.match_bracket + PAIRED_BRACKETS[self.match_bracket]
 
         if len(lines) > 1:
             pos = 0
 
-        return pos + len(lines[-1]), result
+        return pos + len(lines[-1]), jlines
 
     def check_ast(self, parser, ast_tree):
         pass
 
 
-class PythonExpressionMatcher(PythonLineMatcher):
+class PythonDictMatcher(PythonExpressionBaseMatcher):
+    match_bracket = '{'
+
+    def check_ast(self, parser, line, pos, ast_tree):
+        if not isinstance(ast_tree.body[0].value, (ast.Dict, ast.DictComp)):
+            raise PyhaaSyntaxError(
+                SYNTAX_INFO.INVALID_PYTHON_ATTRIBUTES,
+                parser,
+            )
+
+
+class PythonExpressionMatcher(PythonExpressionBaseMatcher):
     def check_ast(self, parser, line, pos, ast_tree):
         if not (len(ast_tree.body) == 1 and isinstance(ast_tree.body[0], ast.Expr)):
             raise PyhaaSyntaxError(
