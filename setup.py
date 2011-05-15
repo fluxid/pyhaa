c108e26ff28c95c48c33151ff952fe9c288bd555
Tomasz Fluxid Kowalczyk <accreg-git@fluxid.pl>
2011-05-15 18:17:53 +0200

    'Flatten' file directories a bit - get rid of 'src' dir.

--- setup.py
+++ setup.py
@@ -8,10 +8,7 @@ if sys.version_info < (3,):
 setup(
     name = 'pyhaa',
     version = '0.0',
-    package_dir = {
-        '': 'src',
-    },
-    packages = find_packages('src'),
+    packages = ['pyhaa'],
     author = 'Tomasz Kowalczyk',
     author_email = 'myself@fluxid.pl',
     description = 'Standalone templating system based on ideas taken from HAML and Mako',
