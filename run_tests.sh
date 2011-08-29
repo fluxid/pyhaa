1fe2e571e5f4c89bc71c3f194e802c9501f46943
Tomasz Fluxid Kowalczyk <accreg-git@fluxid.pl>
2011-08-30 00:40:18 +0200

    Don't give `test` folder to nosetests

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@
 WORKING_DIR="$(cd "${0%/*}" 2>/dev/null; dirname "$PWD"/"${0##*/}")"
 cd ${WORKING_DIR}
 
-nosetests --with-coverage --cover-package=pyhaa --doctest-tests --with-doctest $@ tests
+nosetests --with-coverage --cover-package=pyhaa --doctest-tests --with-doctest $@ 
